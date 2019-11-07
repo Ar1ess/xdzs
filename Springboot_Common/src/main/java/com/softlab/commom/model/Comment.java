355f5b7c18e002e7a02fca6b7933327b2131c393
Aries <925900181@qq.com>
2019-11-07 19:34:25 +0800

    User

---
+++ Springboot_Common/src/main/java/com/softlab/commom/model/Comment.java
@@ -0,0 +1,15 @@
+package com.softlab.commom.model;
+
+import lombok.Data;
+
+/**
+ * @author : Ar1es
+ * @date : 2019/11/7
+ * @since : Java 8
+ */
+
+@Data
+public class Comment {
+
+
+}
--- Springboot_Provider/src/main/java/com/softlab/provider/ProviderApplication.java
+++ Springboot_Provider/src/main/java/com/softlab/provider/ProviderApplication.java
@@ -1,4 +1,4 @@
-package com.softlab.commom.provider;
+package com.softlab.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
