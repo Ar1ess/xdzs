ee4ee5479134a4c4f802a8eb1e49ebc725e57dc1
Aries <925900181@qq.com>
2019-11-07 19:17:35 +0800

    first

--- Springboot_Customer/src/main/java/com/softlab/commom/CustomerApplication.java
+++ Springboot_Customer/src/main/java/com/softlab/commom/CustomerApplication.java
@@ -1,4 +1,4 @@
-package com.softlab.customer;
+package com.softlab.commom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Springboot_Provider/src/main/java/com/softlab/commom/provider/ProviderApplication.java
+++ Springboot_Provider/src/main/java/com/softlab/commom/provider/ProviderApplication.java
@@ -1,4 +1,4 @@
-package com.softlab.customer.provider;
+package com.softlab.commom.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
