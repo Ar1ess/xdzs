fb29648cd24df475803328cf6408a44c6c0564da
LiXiwen <2159559677@qq.com>
2019-11-08 10:45:53 +0800

    lxw branch

---
+++ Springboot_Provider/src/main/java/com/softlab/provider/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.softlab.provider.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.softlab.common.service.UserService;
+
+/**
+ * @author LiXiwen
+ * @date 2019/11/7 20:14
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+}
